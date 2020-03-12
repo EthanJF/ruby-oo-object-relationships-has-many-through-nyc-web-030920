99aabf0ba97717d5cc1da59fd698b171ad427666
Ethan Fournier <ethanjosephfournier@gmail.com>
2020-03-12 15:09:00 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,17 @@
 class Customer
   
+  attr_accessor :name, :age
+  
+  @@all = []
+  
+  def initialize(name,age)
+    @name = name
+    @age = age
+    Customer.all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
 end
